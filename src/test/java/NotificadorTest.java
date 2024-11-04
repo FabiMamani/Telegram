2d703782008ee306ba4d44cb21541b88c850fca3
FabianMamani <fabian.mamani@hotmail.com>
2024-11-04 11:51:02 -0300

    delete Email class

--- src/test/java/NotificadorTest.java
+++ src/test/java/NotificadorTest.java
@@ -1,9 +1,6 @@
 
 import classroom.notifier.implement.MedioComunicacion;
-import notificadores.Email;
 import notificadores.Telegram;
-import notificadores.Whatsapp;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
@@ -14,12 +11,4 @@ public class NotificadorTest {
         notificador = new Telegram();
         notificador.Notificar("PP2","A0102", null);
     }
-
-    @Test
-    public void EmailTest(){
-        notificador = new Email();
-        notificador.Notificar("Taller de Tesina","7075", null);
-    }
-
-
 }
